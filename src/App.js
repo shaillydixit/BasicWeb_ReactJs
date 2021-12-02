ebf95b6d82b7c0958577c3d8a2dd56b608406263
shaillydixit <76932875+shaillydixit@users.noreply.github.com>
2021-12-03 04:38:47 +0530

    functional to class

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import React,{useState} from 'react';
+import React,{Component} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-// state hook class to functional compo
-export const App = () => {
-  const [count, increaseCount] = useState(9)
+// state hook functional to class compo
+class App extends Component{
+  state={
+    count: 0
+  }
+
+  increase = () => [
+    this.setState({count:this.state.count +4})
+  ]
+render(){
   return(
     <div>
-      <h1>Functional component {count}</h1>
-      <button onClick={(e)=>{{increaseCount(count+3)}}}>Click Me </button>
+      <h1>Time...{this.state.count}</h1>
+     <button onClick={this.increase}>Click me</button>
     </div>
   )
+}
 }
 export default App;
