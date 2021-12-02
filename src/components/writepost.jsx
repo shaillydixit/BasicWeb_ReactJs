61e41fe3db2c6977fec24be2d1259b48cf5d4a1b
shaillydixit <76932875+shaillydixit@users.noreply.github.com>
2021-12-03 04:03:14 +0530

    http post data

--- src/components/writepost.jsx
+++ src/components/writepost.jsx
@@ -1,19 +1,38 @@
 import React,{ Component } from 'react';
-
+import axios from 'axios';
 export default class post extends Component{
+    state = {
+        title: '',
+        body: ''
+    }
+    formSubmit = (e) => {
+        e.preventDefault();
+        axios.post('/writepost', { //where it will be redirected
+            title: this.state.title,
+            body: this.state.body
+          })
+          .then(function (response) {
+            console.log(response);
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+    }
 render(){
 return(
     <center>
 <div>
     <div class="jumbotron col-lg-4">
-        <form>
+        <form onSubmit={this.formSubmit}>
             <div class="form-group">
                 <label for="exampleInputEmail1">Title</label>
-                <input type="text" class="form-control" id="title" name="title" required />
+                <input type="text" class="form-control" id="title" name="title"
+                 required onChange={(e)=>{this.setState({title:e.target.value})}}/>
             </div>
             <div class="form-group">
                 <label for="exampleInputPassword1" >Details</label>
-                <textarea class="form-control" name="body" cols="30" rows="10">Write Here</textarea>
+                <textarea class="form-control" name="body" cols="30" rows="10"
+                onChange={(e)=>{this.setState({body:e.target.value})}}>Write Here</textarea>
             </div>
             <button type="submit" class="btn btn-primary">Publish</button>
         </form>
