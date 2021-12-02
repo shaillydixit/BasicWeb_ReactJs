b93015218e10c348f3997f87e23ea9e4ed77417f
shaillydixit <76932875+shaillydixit@users.noreply.github.com>
2021-12-03 00:20:12 +0530

    fetch data from API

--- src/components/blog.jsx
+++ src/components/blog.jsx
@@ -1,12 +1,47 @@
 import React,{ Component } from 'react';
-
+// const axios = require('axios');
+import axios from 'axios';
 export default class blog extends Component{
+
+    state= {
+        posts:[],
+    }
+    componentDidMount(){
+// let thisvar = this; if you using function keyword
+//    alert('hello alert'); 
+// Make a request for a user with a given ID
+axios.get('https://jsonplaceholder.typicode.com/posts')
+  .then((response) => { // handle success
+    // console.log(response);
+    // thisvar.setState({posts:response.data})
+    this.setState({posts:response.data})
+  })
+  .catch(function (error) {     // handle error
+    console.log(error);
+  })
+}
     render(){
+        const posts = this.state.posts;
+        const allposts = posts.map((post,idx) =>{
+            return(
+                <div>
+                    <a href="" key={idx}>{post.title}</a> <br></br>
+                <p>{post.body}</p>
+                </div>
+            )
+        })
         return (
             <div>
                 <h1>
                     This is blog page
                 </h1>
+     <div class="media">
+
+  <div class="media-body text-center">
+  {allposts}
+  </div>
+
+</div>
             </div>
         )
     }
