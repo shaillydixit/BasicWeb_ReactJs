b83e5ce80010826b7f2f54f4a45db61fb0f46463
shaillydixit <76932875+shaillydixit@users.noreply.github.com>
2021-12-03 00:44:45 +0530

    form created

--- src/components/writepost.jsx
+++ src/components/writepost.jsx
@@ -1,13 +1,25 @@
 import React,{ Component } from 'react';
 
 export default class post extends Component{
-    render(){
-        return(
+render(){
+return(
+    <center>
 <div>
-    <h1>
-        Write Post here...
-    </h1>
+    <div class="jumbotron col-lg-4">
+        <form>
+            <div class="form-group">
+                <label for="exampleInputEmail1">Title</label>
+                <input type="text" class="form-control" id="title" name="title" required />
+            </div>
+            <div class="form-group">
+                <label for="exampleInputPassword1" >Details</label>
+                <textarea class="form-control" name="body" cols="30" rows="10">Write Here</textarea>
+            </div>
+            <button type="submit" class="btn btn-primary">Publish</button>
+        </form>
+    </div>
 </div>
-        )
-    }
+</center>
+)
+}
 }
