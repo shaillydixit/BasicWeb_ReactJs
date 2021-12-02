6d8bdc7da02343b9eebe3b3c8ca5836c14f1fef8
shaillydixit <76932875+shaillydixit@users.noreply.github.com>
2021-12-03 04:24:57 +0530

    for active link

--- src/header.jsx
+++ src/header.jsx
@@ -2,7 +2,7 @@ import React,{Component} from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
-import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route, NavLink} from "react-router-dom";
 import About from './components/about';
 import Blog from './components/blog';
 import Contact from './components/contact';
@@ -17,22 +17,22 @@ return(
     <Navbar bg="light" expand="lg">
       <Container fluid>
         <Navbar.Brand>
-          <Link to="/">Shailly Dixit</Link>
+          <NavLink exact to="/" activeStyle={{color:'red'}}>Shailly Dixit</NavLink>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav className="me-auto my-2 my-lg-0" style={{ maxHeight: '100px' }} navbarScroll>
             <Nav.Link>
-              <Link to="/">Home</Link>
+              <NavLink to="/" activeStyle={{color:'red'}}>Home</NavLink>
             </Nav.Link>
             <Nav.Link>
-              <Link to="/about">About</Link>
+              <NavLink to="/about" activeStyle={{color:'red'}}>About</NavLink>
             </Nav.Link>
             <Nav.Link>
-              <Link to="/blog">Blog</Link>
+              <NavLink to="/blog" activeStyle={{color:'red'}}>Blog</NavLink>
             </Nav.Link>
             <Nav.Link>
-              <Link to="/contact">Contact</Link>
+              <NavLink to="/contact" activeStyle={{color:'red'}}>Contact</NavLink>
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
@@ -40,14 +40,14 @@ return(
     </Navbar>
   </div>
   <Routes>
-    <Route path='/about' element={<About/>}/>
-    <Route path='/blog' element={<Blog/>}/>
-    <Route path='/contact' element={<Contact/>}/>
-    <Route path='/' element={<App/>}/>
-    <Route path='/post' element={<Post/>}/>
+    <Route path='/about' element={<About />}/>
+    <Route path='/blog' element={<Blog />}/>
+    <Route path='/contact' element={<Contact />}/>
+    <Route path='/' element={<App />}/>
+    <Route path='/post' element={<Post />}/>
 
   </Routes>
-  </Router>
+</Router>
 
 
 )
